54d65db1b0f4d72a6f08dd66f2ce4bd6f44fe758
Clement <clementnyankuadjei@gmail.com>
2025-06-08 22:20:03 +0000

    Add header and sidebar components, update styles, and configure analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderNavComponent } from "./header/header-nav/header-nav.component";
+import { SideBarComponent } from "./header/side-bar/side-bar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderNavComponent, SideBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
